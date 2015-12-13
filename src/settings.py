e3628462c072686788898a8d65b9d1ab7be58685
Miha Zidar <zidarsk8@gmail.com>
2015-12-13 02:07:06 +0100

    Update settings for karma and psywerx

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,9 @@
 """Created on Oct 9, 2011"""
 TOKEN = '16edde56d1801c65ec96a4d607a67d89'
-SERVER_URL = 'http://localhost:8000/'
-BOT_NICKS = ['_santa_test_', 'santa__test_']
+SERVER_URL = 'http://psywerx.org/'
+BOT_NICKS = ['_santa_', '_santa__', '_santa_test_', 'santa__test_']
 BOT_NAME = 'santa'
-CHANNELS = ['#santa-testing', '#santa-testing2']
+CHANNELS = ['#psywerx', '#santa-testing', '#santa-testing2']
 IRC_SERVER = 'irc.freenode.org'
 IRC_PORT = 6667
 COOKIEZ_URL = 'http://www.fortunefortoday.com/getfortuneonly.php'
@@ -11,7 +11,7 @@ SIMON_USERS = ['smotko', 'edofic']
 
 PSYWERX = {
     'TOKEN': '16edde56d1801c65ec96a4d607a67d89',
-    'SERVER_URL': 'http://localhost:8000/',
+    'SERVER_URL': 'http://psywerx.org/',
 }
 
 TWITTER = {
